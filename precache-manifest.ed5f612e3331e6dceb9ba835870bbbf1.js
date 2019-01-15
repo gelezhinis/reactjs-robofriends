aca7c04a444622b1aeea4ec858e875fc3d297aa4
gelezhinis <gelezhinis@gmail.com>
2019-01-15 11:01:29 +0200

    Updates

---
+++ precache-manifest.ed5f612e3331e6dceb9ba835870bbbf1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/reactjs-robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "7511e8b9d5a7a184eacf",
+    "url": "/reactjs-robofriends/static/js/runtime~main.7511e8b9.js"
+  },
+  {
+    "revision": "3a1a59b19534b85bcd3f",
+    "url": "/reactjs-robofriends/static/js/main.3a1a59b1.chunk.js"
+  },
+  {
+    "revision": "98a3c91f7cbd70c84ca3",
+    "url": "/reactjs-robofriends/static/js/1.98a3c91f.chunk.js"
+  },
+  {
+    "revision": "3a1a59b19534b85bcd3f",
+    "url": "/reactjs-robofriends/static/css/main.aa1b0a92.chunk.css"
+  },
+  {
+    "revision": "98a3c91f7cbd70c84ca3",
+    "url": "/reactjs-robofriends/static/css/1.633fa0b2.chunk.css"
+  },
+  {
+    "revision": "e83b906dc0eba607d7020f086c2602e7",
+    "url": "/reactjs-robofriends/index.html"
+  }
+];
